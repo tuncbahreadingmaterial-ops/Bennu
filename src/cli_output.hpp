30e732a2ca9ee8749465da073b8dafc20ada1a89
tuncbahreadingmaterial-ops <306473718+tuncbahreadingmaterial-ops@users.noreply.github.com>
2026-07-19 23:51:18 +0200

    test: use data-oriented stdout failure seam (#17)

--- src/cli_output.hpp
+++ src/cli_output.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
+#include <cstddef>
 #include <iosfwd>
 #include <string_view>
 
 namespace bennu_cli {
 
+struct OutputWriteResult {
+  bool ok;
+  std::size_t byte_count;
+};
+
+using OutputWriteFunction = OutputWriteResult (*)(void *, std::string_view);
+using OutputFlushFunction = bool (*)(void *);
+
+struct OutputSink {
+  void *context;
+  OutputWriteFunction write;
+  OutputFlushFunction flush;
+};
+
+bool write_stdout(OutputSink output, std::string_view text);
+bool flush_stdout(OutputSink output);
 bool write_stdout(std::ostream &output, std::string_view text);
 bool flush_stdout(std::ostream &output);
 
